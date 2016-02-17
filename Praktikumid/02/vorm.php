b40d3cd1e3caedb8bf95a67dc7620ab993c061ce
Andris Reinman <andris@kreata.ee>
2016-02-17 18:46:50 +0200

    lisatud praktikumide harjutused

---
+++ Praktikumid/02/vorm.php
@@ -0,0 +1,6 @@
+<?php
+    // anna brauserile teada, et tegu on tekstiväljundiga
+    header('content-type: text/plain; charset=utf-8');
+    // kuva ekraanil POST päringu sisu
+    var_dump($_POST);
+?>
